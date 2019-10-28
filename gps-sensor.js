30c5636b6a3064149f4493fd8c1be966da8459c8
Benjamin Davies <bentendavies@gmail.com>
2019-10-28 20:46:18 +1300

    Switch to using Firebase's GeoPoint objects

--- gps-sensor.js
+++ gps-sensor.js
@@ -1,61 +1,36 @@
+const { firestore: { GeoPoint } } = require('firebase');
+
 class GpsSensor {
   constructor() {
-    this.loc = undefined;
+    this.loc = null;
   }
 
   process(data) {
     if (data.talker === 'GN') {
       if (data.messageType === 'GLL') {
-        this.loc = Location.fromNmea(data.fields);
+        this.loc = pointFromNmea(data.fields);
       }
     }
   }
 }
 
-class Location {
-  constructor(latitude = new LocationAxis(), longitude = new LocationAxis()) {
-    this.latitude = latitude;
-    this.longitude = longitude;
-  }
-
-  toString() {
-    return `${this.latitude} ${this.longitude}`;
-  }
-
-  /**
-   * @param{string[]} fields
-  **/
-  static fromNmea(fields) {
-    return new Location(
-      LocationAxis.fromNmea(...fields.slice(0, 2)),
-      LocationAxis.fromNmea(...fields.slice(2, 4)),
-    );
-  }
+/**
+ * @param{string[]} fields
+ **/
+function pointFromNmea(fields) {
+  return new GeoPoint(
+    axisFromNmea(...fields.slice(0, 2)),
+    axisFromNmea(...fields.slice(2, 4)),
+  );
 }
 
-class LocationAxis {
-  constructor(direction = 'N', degrees = 0, minutes = 0) {
-    this.direction = direction;
-    this.degrees = degrees;
-    this.minutes = minutes;
-  }
-
-  toString() {
-    return `${this.degrees} ${this.minutes.toFixed(5)}${this.direction}`;
-  }
-
-  /**
-   * @param{string} v1
-   * @param{string} v2
-  **/
-  static fromNmea(v1, v2) {
-    const n = parseFloat(v1);
-    return new LocationAxis(
-      v2,
-      Math.floor(n / 100),
-      n % 100,
-    );
-  }
+function axisFromNmea(v1, v2) {
+  const n = parseFloat(v1);
+  const sign = v2 === 'N' || v2 == 'E'
+    ? 1 : -1;
+  const deg = Math.floor(n / 100);
+  const min = n % 100;
+  return sign*(deg + min/60);
 }
 
 module.exports = GpsSensor;
