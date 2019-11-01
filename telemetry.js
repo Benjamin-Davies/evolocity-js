620a69135128b5a7aaf7cae718225ab7fd13674c
Benjamin Davies <bentendavies@gmail.com>
2019-11-01 16:17:03 +1300

    Add speed metric

--- telemetry.js
+++ telemetry.js
@@ -53,9 +53,9 @@ function gatherData() {
     time: new Date(),
     current: motor.current,
     voltage: motor.voltage,
-    speed: 0,
     battery_voltage: battery.voltage,
     location: gps.loc || null,
+    speed: gps.speed,
   };
 }
 
